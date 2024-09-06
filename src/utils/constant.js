b32a3406fb53e435ebecd4dcbab8b8fbd06d1781
shihabb556 <shihabaly566@gmail.com>
2024-09-06 07:09:31 +0600

    fixing server url

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,5 +1,5 @@
-export const USER_API_END_POINT="https://job-portal-server-01.vercel.app/api/v1/";
-export const JOB_API_END_POINT="https://job-portal-server-01.vercel.app/api/v1/";
+export const USER_API_END_POINT="https://job-portal-server-01.vercel.app/api/v1";
+export const JOB_API_END_POINT="https://job-portal-server-01.vercel.app/api/v1";
 export const APPLICATION_API_END_POINT="https://job-portal-server-01.vercel.app/api/v1";
 export const COMPANY_API_END_POINT="https://job-portal-server-01.vercel.app/api/v1";
 export const BASE_URL = "https://job-portal-server-01.vercel.app/api/v1";
